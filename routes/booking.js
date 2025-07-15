999034ac245a845d34613be2178c1c5ddeb12962
Pankaj Kandpal <pankaj.kandpal24@gmail.com>
2025-07-15 21:04:29 +0530

    cancel booking error resolved

--- routes/booking.js
+++ routes/booking.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 const Booking = require("../models/booking.js"); // adjust path if needed
 const Listing = require("../models/listing.js");
-const {isLoggedIn}=require("../middleware.js");
+const { isLoggedIn } = require("../middleware.js");
 
 
 const preventOwnerBooking = async (req, res, next) => {
@@ -17,46 +17,46 @@ const preventOwnerBooking = async (req, res, next) => {
 
 
 
-router.get("/bookings/:id",isLoggedIn, async (req, res) => {
-    const { id } = req.params;
+router.get("/bookings/:id", isLoggedIn, async (req, res) => {
+  const { id } = req.params;
 
-    try {
-        const listing = await Listing.findById(id).populate("owner");
-        if (!listing) {
-            req.flash("error", "Listing not found");
-            return res.redirect("/listings");
-        }
-        res.render("bookings/bookingForm", { listing ,currentUser: req.user});
-    } catch (err) {
-        console.error("Error fetching listing for booking:", err);
-        req.flash("error", "Something went wrong!");
-        res.redirect("/listings");
+  try {
+    const listing = await Listing.findById(id).populate("owner");
+    if (!listing) {
+      req.flash("error", "Listing not found");
+      return res.redirect("/listings");
     }
+    res.render("bookings/bookingForm", { listing, currentUser: req.user });
+  } catch (err) {
+    console.error("Error fetching listing for booking:", err);
+    req.flash("error", "Something went wrong!");
+    res.redirect("/listings");
+  }
 });
 
-router.post("/bookings",isLoggedIn,preventOwnerBooking, async (req, res) => {
-    const { userId, listingId, checkIn, checkOut,guests, totalPrice } = req.body;
-    console.log(req.body);
+router.post("/bookings", isLoggedIn, preventOwnerBooking, async (req, res) => {
+  const { userId, listingId, checkIn, checkOut, guests, totalPrice } = req.body;
+  console.log(req.body);
 
-    if (!userId || !listingId || !checkIn || !checkOut || !totalPrice) {
-        return res.status(400).json({ error: "Missing required fields" });
-    }
+  if (!userId || !listingId || !checkIn || !checkOut || !totalPrice) {
+    return res.status(400).json({ error: "Missing required fields" });
+  }
 
-    try {
-        await Booking.save({
-            user: userId,
-            listing: listingId,
-            checkIn: new Date(checkIn),
-            checkOut: new Date(checkOut),
-            guests: guests,
-            totalPrice,
-        });
+  try {
+    await Booking.save({
+      user: userId,
+      listing: listingId,
+      checkIn: new Date(checkIn),
+      checkOut: new Date(checkOut),
+      guests: guests,
+      totalPrice,
+    });
 
-        res.redirect("/my/bookings")
-    } catch (err) {
-        console.error(err);
-        res.status(500).json({ error: "Booking failed" });
-    }
+    res.redirect("/my/bookings")
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: "Booking failed" });
+  }
 });
 
 router.get("/my/bookings", isLoggedIn, async (req, res) => {
@@ -71,22 +71,22 @@ router.delete("/bookings/:id", isLoggedIn, async (req, res) => {
     const booking = await Booking.findById(req.params.id);
     if (!booking) {
       req.flash("error", "Booking not found");
-      return res.redirect("bookings/mybookings");
+      return res.redirect("my/bookings");
     }
 
     // Allow only the user who created the booking to cancel
     if (!booking.user.equals(req.user._id)) {
       req.flash("error", "Unauthorized");
-      return res.redirect("bookings/mybookings");
+      return res.redirect("my/bookings");
     }
 
     await Booking.findByIdAndDelete(req.params.id);
     req.flash("success", "Booking cancelled successfully");
-    res.redirect("bookings/mybookings");
+    res.redirect("my/bookings");
   } catch (err) {
     console.error(err);
     req.flash("error", "Something went wrong while cancelling");
-    res.redirect("bookings/mybookings");
+    res.redirect("my/bookings");
   }
 });
 
